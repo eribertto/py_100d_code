a242835fa6d57aa5e09543df1ac369c26180f988
erimendz_fed36_cinammon <erimendz@gmail.com>
2023-01-15 19:23:39 +0300

    add nested if elif exercise

---
+++ nested_if_rollercoaster.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+'''
+using nested if elif statements to check the height and age limit of the user
+
+'''
+
+ask_user = int(input("Please enter your height in cm: "))
+if ask_user >= 120:
+    print("Yup you can ride the rollercoaster")
+    get_age = int(input("What is your age? "))
+    if get_age < 12:
+        print("Please pay $5.")
+    elif get_age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
