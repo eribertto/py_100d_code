06ed7fcd6f5c2e3a350fc02c804772a9ce373fe2
erimendz_fed36_cinammon <erimendz@gmail.com>
2023-01-16 19:57:04 +0300

    add is-leap-year crude script

---
+++ is_leap_year.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# check if a year is leap year or no
+# https://stackoverflow.com/questions/11621740/how-to-determine-whether-a-year-is-a-leap-year
+
+import calendar
+year = input("Enter the year to check for leap year: ")
+if year.isdigit():
+    year = int(year)
+else:
+    print("Invalid input")
+    exit
+    
+if calendar.isleap(year):
+    print(f"{year} is a leap year!")
+else:
+    print("Nope not a leap year")
+
+# note: use error exception handling here
