a34f2f09a534664ab1e0216ba1c579f1956d7e4c
erimendz_fed36_cinammon <erimendz@gmail.com>
2023-01-17 05:31:36 +0300

    refine the else if logic

--- leap_year.check.py
+++ leap_year.check.py
@@ -5,14 +5,12 @@
 year = int(input("Enter the year to check for leap year: "))
 
 if year % 4 == 0:
-#     print(f"{year} is a leap year")
     if year % 100 == 0:
-#         print(f"{year} is a leap year")
         if year % 400 == 0:
             print(f"{year} is a leap year")
         else:
-            print(f"{year} is NO leap year")
+            print(f"{year} is NOT a leap year")
     else:
-        print(f"{year} is NO leap year")
+        print(f"{year} is a leap year")
 else:
-    print(f"{year} is NO leap year")
+    print(f"{year} is NOT leap year")
